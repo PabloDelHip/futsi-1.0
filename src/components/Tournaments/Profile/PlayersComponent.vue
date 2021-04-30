252a0c8a25941a68bfc915cd1c6b57120491189a
pablodelhip <pablodelhip@gmail.com>
2021-04-30 18:10:25 -0500

    feature: tabla estadisticas

--- src/components/Tournaments/Profile/PlayersComponent.vue
+++ src/components/Tournaments/Profile/PlayersComponent.vue
@@ -1,5 +1,200 @@
 <template>
-    <div class="row col-10 contenedor-info-tournament p-0">
-        JUGADORES
+    <div class="row col-12 justify-content-center">
+        <div class="col-10 contenedor-info mb-3">
+            <div class="card-body">
+                <h5 class="titulo-medio-pequeño-azul">Tabla de goleo</h5>
+                <table id="dataTable" class="table table-bordered table-striped tabla-posiciones">
+                  <thead>
+                    <tr>
+                        <th class="table-posicion">Pos</th>
+                        <th class="table-equipos">Jugador</th>
+                        <th class="table-posicion">PJ</th>
+                        <th class="table-puntos">Goles</th>
+                    </tr>
+                  </thead>
+                    <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                    </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+        </div>
+
+        <div class="col-10 contenedor-info mb-3">
+            <div class="card-body">
+                <h5 class="titulo-medio-pequeño-azul">Mejores porteros</h5>
+                <table id="dataTable" class="table table-bordered table-striped tabla-posiciones">
+                  <thead>
+                    <tr>
+                        <th class="table-posicion">Pos</th>
+                        <th class="table-equipos">Jugador</th>
+                        <th class="table-posicion">PJ</th>
+                        <th class="table-puntos">Goles</th>
+                    </tr>
+                  </thead>
+                    <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                    </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+        </div>
+
+        <div class="col-10 contenedor-info mb-3">
+            <div class="card-body">
+                <h5 class="titulo-medio-pequeño-azul">Tarjetas amarillas</h5>
+                <table id="dataTable" class="table table-bordered table-striped tabla-posiciones">
+                  <thead>
+                    <tr>
+                        <th class="table-posicion">Pos</th>
+                        <th class="table-equipos">Jugador</th>
+                        <th class="table-posicion">PJ</th>
+                        <th class="table-puntos">Goles</th>
+                    </tr>
+                  </thead>
+                    <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                    </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+        </div>
+
+        <div class="col-10 contenedor-info mb-3">
+            <div class="card-body">
+                <h5 class="titulo-medio-pequeño-azul">Tarjetas rojas</h5>
+                <table id="dataTable" class="table table-bordered table-striped tabla-posiciones">
+                  <thead>
+                    <tr>
+                        <th class="table-posicion">Pos</th>
+                        <th class="table-equipos">Jugador</th>
+                        <th class="table-posicion">PJ</th>
+                        <th class="table-puntos">Goles</th>
+                    </tr>
+                  </thead>
+                    <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td class="tabla-nombre-equipo"><b>Jose Perez</b></td>
+                          <td>12</td>
+                          <td>10</td>
+                        </tr>
+                    </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+        </div>
+    
     </div>
 </template>
